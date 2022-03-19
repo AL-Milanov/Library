f18a4ccc20671980628d399b9587139b36e8b0c4
AL-Milanov <alexandermilanov161195@gmail.com>
2022-03-19 21:56:30 +0200

    added application user model

--- Library.Infrastructure/Data/ApplicationDbContext.cs
+++ Library.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Library.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Library.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+
     }
 }
