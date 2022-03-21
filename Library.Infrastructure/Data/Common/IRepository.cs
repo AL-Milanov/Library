e68a871b6fbb6f2cef4b93ada445ab6ebbc8b4c2
AL-Milanov <alexandermilanov161195@gmail.com>
2022-03-21 22:40:15 +0200

    modified GetAll method and GetByIdAsync

--- Library.Infrastructure/Data/Common/IRepository.cs
+++ Library.Infrastructure/Data/Common/IRepository.cs
@@ -4,11 +4,11 @@ namespace Library.Infrastructure.Data.Common
 {
     public interface IRepository
     {
-        Task<IQueryable<T>> GetAllAsync<T>() where T : class;
+        IQueryable<T> GetAll<T>() where T : class;
 
-        Task<T> GetByIdAsync<T>(string id) where T : class;
+        Task<T> GetByIdAsync<T>(Expression<Func<T, bool>> search) where T : class;
 
-        Task<T> GetByParameterAsync<T>(Expression<Func<string, T>> search) where T : class;
+        Task<T> GetByParameterAsync<T>(Expression<Func<T, bool>> search) where T : class;
 
         Task SaveChangesAsync();
     }
