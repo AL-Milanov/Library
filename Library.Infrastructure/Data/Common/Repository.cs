1eabdf1bfe312cfd159f59b9b0b3f9d5a9eab418
AL-Milanov <alexandermilanov161195@gmail.com>
2022-03-21 22:40:41 +0200

    implemented Repository class

---
+++ Library.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Library.Infrastructure.Data.Common
+{
+    public class Repository : IRepository
+    {
+        protected DbContext Context { get; set; }
+
+        protected DbSet<T> DbSet<T>() where T : class
+        {
+            return Context.Set<T>();
+        }
+
+        public IQueryable<T> GetAll<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public async Task<T> GetByIdAsync<T>(Expression<Func<T, bool>> search) where T : class
+        {
+            return await DbSet<T>()
+                .FindAsync(search);
+        }
+
+        public async Task<T> GetByParameterAsync<T>(Expression<Func<T, bool>> search) where T : class
+        {
+            return await DbSet<T>()
+                .Where(search)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await Context.SaveChangesAsync();
+        }
+
+    }
+}
