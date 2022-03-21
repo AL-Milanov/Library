30fd0b40fcf0f6a7d2f61bbfc594fb4032f71ef2
AL-Milanov <alexandermilanov161195@gmail.com>
2022-03-21 22:25:07 +0200

    added IRepository interface

---
+++ Library.Infrastructure/Data/Common/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Library.Infrastructure.Data.Common
+{
+    public interface IRepository
+    {
+        Task<IQueryable<T>> GetAllAsync<T>() where T : class;
+
+        Task<T> GetByIdAsync<T>(string id) where T : class;
+
+        Task<T> GetByParameterAsync<T>(Expression<Func<string, T>> search) where T : class;
+
+        Task SaveChangesAsync();
+    }
+}
